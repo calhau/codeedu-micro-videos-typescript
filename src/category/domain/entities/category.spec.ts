8d658baf5d757fc73979ae19b02a1050b017a9c4
Bruno Calhau <lealbrunocalhau@gmail.com>
2022-03-30 20:01:50 -0300

    adicionando mais testes em categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -14,6 +14,34 @@ describe('Category testes', ()=> {
                 is_active: true,
             }
         )
+
+        expect(category.props.created_at).toBeInstanceOf(Date);
+
+        category = new Category({name: 'Movie', description: "some description", is_active: false})
+        let created_at = new Date()
+
+        expect(category.props).toStrictEqual({
+            name: "Movie",
+            description: "some description",
+            is_active: false,
+            created_at
+        })
+
+        category = new Category({name: 'Movie', description: "other description"})
+
+        expect(category.props).toMatchObject({
+            name: "Movie",
+            description: "other description",
+        })
+
+
+        category = new Category({name: 'Movie', is_active: true})
+
+        expect(category.props).toMatchObject({
+            name: "Movie",
+            is_active: true
+        })
+
         // expect(category.name).toBe('Movie');
         // expect(category.description).toBe('some description');
         // expect(category.is_active).toBeTruthy();
