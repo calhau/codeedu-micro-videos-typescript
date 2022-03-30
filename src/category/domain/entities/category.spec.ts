2d65bb86777eb00cd937c7e5773775d29d5843b1
Bruno Calhau <lealbrunocalhau@gmail.com>
2022-03-30 19:12:48 -0300

    outro tipo de teste

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,10 +1,20 @@
 import { Category } from "./category"
 
 describe('Category testes', ()=> {
-     
     test('constructor of category', () => {
-        const category = new Category('Movie'); 
+
+        const props = {
+            name: "Movie",
+            description: "some description",
+            is_active: true,
+            created_at: new Date(),
+        }
+        const created_at = new Date();
+        const category = new Category(props); 
         
-        expect(category.name).toBe('Movie')
+        expect(category.name).toBe('Movie');
+        expect(category.description).toBe('some description');
+        expect(category.is_active).toBeTruthy();
+        expect(category.created_at).toBe(created_at)
     })
 }) 
